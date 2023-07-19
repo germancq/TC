6b7802a0cb4a7e07dfae7a0978e4b20f6a589643
germancq <germancq@gmail.com>
2023-07-19 11:18:06 +0200

    script

---
+++ install_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#install visual studio code
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+#install platformIO
+
+code --install-extension platformio.platformio-ide
+
+#install pip3
+sudo apt install python3-pip
+
+#install python packages
+pip3 install --user pyserial
+pip3 install --user matplotlib
+pip3 install --user numpy
+
